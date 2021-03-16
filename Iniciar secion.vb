7d8e7a1a82d7008cc1a523fb1fdd6457c6801658
Yeim1129 <79933243+Yeim1129@users.noreply.github.com>
2021-03-16 04:00:08 -0600

    Delete Iniciar secion.vb

--- Iniciar secion.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Iniciar_secion
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox3.TextChanged, TextBox2.TextChanged
-
-    End Sub
-End Class
